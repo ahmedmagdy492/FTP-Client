0d6d2ea7be773d24a4e6d7b0e68363b5f617e35c
ahmed magdy <ahmedmagdy492@yahoo.com>
2022-03-27 19:12:47 +0200

    initial design

---
+++ Pages/Connections.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FTP_Client.Pages
+{
+    public class ConnectionsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
